a3b660d64f20373729dce02066983ee75b3ea7e3
aaron.delacruz <aaron.delacruz@unistarcredit.com.ph>
2023-03-10 22:14:41 +0800

    added file headers and generated requirements.txt

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,14 @@
+__author__ = "Deogracias Aaron P. Dela Cruz"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
+ "Matthew Wakefield"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Rob Knight"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Production"
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
